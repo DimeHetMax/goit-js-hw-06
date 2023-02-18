9a3ff5dbedc1ccd27726f2000615ef110c5ec977
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-02-18 23:44:26 +0200

    second task
    got "ul" element by id,  created li with creatElement().
    Used "for ... of" loop to Iterate array, created  "li" element, added classlist, textContent to each "li". Then used method append() to add all elements to "ul".

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients')
+console.log(ulEl);
+
+for (const ingredient of ingredients){
+  const liEl = document.createElement('li');
+  liEl.textContent = `${ingredient}`;
+  liEl.classList.add('item');
+  ulEl.append(liEl);
+}
