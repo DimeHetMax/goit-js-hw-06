3eb4b8c07695b640cca957e2291c6d2f273f83dc
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-02-19 22:09:34 +0200

    ninth task
    1. Got refs for a button and a span.
    2. Set addEventListener() on the button with  the "click" event.
    3. Created the variable "body"
    4. Created function "onClickButton".
    5. Сall a function "getRandomHexColor" inside of function "onClickButton";

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body.style;
+
+const refs = {
+  buttonEl: document.querySelector(".change-color"),
+  spanEl: document.querySelector(".color"),
+};
+
+refs.buttonEl.addEventListener('click', onClickButton)
+
+function onClickButton(){
+  const result = getRandomHexColor();
+  body.backgroundColor = `${result}`
+  refs.spanEl.textContent = `${result}`
+}
