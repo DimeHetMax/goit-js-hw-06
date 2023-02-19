3708cff80529193e8a7e2ed799fea96062732c9b
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-02-19 21:28:21 +0200

    eighth task
    1. Got the form's link by querySelector().
    2. Added addEventListener() on the form.
    3.Created function " onSubmitButton" and used methid preventDefault().
    4.Got the elements in the variable.
    5. Used condition "If...else"
    6.Created object that saves data from inputs.
    7 reset the fields.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", onSubmitButton)
+
+function onSubmitButton(event) {
+    event.preventDefault()
+    
+    const elements = event.currentTarget.elements;
+    
+    if(elements.email.value === "" || elements.password.value === ""){
+        alert('Все поля должны быть заполнены!');
+        alert('All fields should be filled out!');
+    } else{
+    const email = elements.email.value;
+    const password = elements.password.value;
+
+    const dataObj = {
+            email, 
+            password,
+        }
+    console.log(dataObj)
+    }
+    event.currentTarget.reset();
+}
